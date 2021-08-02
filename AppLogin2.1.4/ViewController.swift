2549b9b8b932194548234ce1eeeb48786cb8ab84
Zakir.Kazimov <ramzan666666@gmail.com>
2021-08-02 18:13:00 +0300

    Initial Commit

---
+++ AppLogin2.1.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLogin2.1.4
+//
+//  Created by Zakir on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
